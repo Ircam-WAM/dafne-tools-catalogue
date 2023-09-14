b667a69a2d789d3a9310627999d67116c2165563
piccarreta <guillaume.piccarreta@ircam.fr>
2023-09-14 18:24:26 +0200

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Component1 from "@/components/Component1/index.vue";
-import Component2 from "@/components/Component2/index.vue";
+import { createWebHistory, createWebHashHistory, createRouter } from "vue-router";
+import toolsApiEndpoints from '@/api/tools-origin.json';
+import Home from "@/components/Home/index.vue";
+import ToolComponent from "@/components/ToolComponent/index.vue";
 
 const routes = [
-    {
-        path: "/component1",
-        name: "component1",
-        component: Component1,
-    },
-    {
-        path: "/component2",
-        name: "component2",
-        component: Component2,
-    },
-];
+    { path: '/', component: Home },
+]
+
+const toolsEndpoints = toolsApiEndpoints["tools_origin"]
+
+// parse routes from api
+for (let i = 0; i < toolsEndpoints.length; i++) {
+    const route = {
+        path: "/tool/:name",
+        name: toolsEndpoints[i].name,
+        component: ToolComponent,
+        props: true,
+    }
+    routes.push(route);
+}
+
+console.log("ALL ROUTES: ", routes);
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: process.env.NODE_ENV === 'development' ? createWebHashHistory() : createWebHistory(),
     routes,
 });
 
