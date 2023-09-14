0c787d0b722e084c45f6fb7a7c4e02a70959f578
piccarreta <guillaume.piccarreta@ircam.fr>
2023-09-14 18:46:25 +0200

    add useToolsApi composable fn

---
+++ src/api/useToolsApi.js
@@ -0,0 +1,29 @@
+import { ref } from "vue"
+import toolsApiEndpoints from '@/api/tools-origin.json';
+import toolsApiData from '@/api/tools-api.json';
+
+const api = ref([])
+
+const toolsEndpoints = toolsApiEndpoints["tools_origin"]
+const toolsApi = toolsApiData["tools_api"]
+
+export default async function useToolsApi () {
+  api.value = []
+
+  const tool = ref()
+
+  for (let i = 0; i < toolsEndpoints.length; i++) {
+
+    // fetch tool api endpoint
+    // const tool = await fetch(toolsEndpoints[i].api_url)
+    // in dev env: fetch local json to emulate api parsing
+    for (let j = 0; j < toolsApi.length; j++) {
+        if (toolsEndpoints[i].name === toolsApi[j].name) {
+            tool.value = toolsApi[j]
+        }
+    }
+    api.value.push(tool.value)
+  }
+
+  return { api }
+}
