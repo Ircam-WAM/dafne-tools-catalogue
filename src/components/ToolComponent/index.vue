e27457341e0a303de33b0c67928e824c2d655c1d
piccarreta <guillaume.piccarreta@ircam.fr>
2023-09-15 13:25:02 +0200

    update tool component

--- src/components/ToolComponent/index.vue
+++ src/components/ToolComponent/index.vue
@@ -1,24 +1,41 @@
 <template>
-    <div class="tool__nav">
+    <!-- <div class="tool__nav">
         <template v-for="tool in tools">
         <router-link :to="'/tool/' + tool.name">{{ tool.name }}</router-link> |
         </template>
-    </div>
-    <div class="tool__container">
+    </div> -->
+    <router-link to="/"><button :class="$style.backButton">↵ back</button></router-link>
+    <div :class="$style.toolContainer">
         <template v-if="selectedTool">
-            <div :class="$style.foo">{{ selectedTool.name }}</div>
-            <iframe
-                v-if="selectedTool.app_url"
-                class="tool-iframe"
-                title="Tool Iframe"
-                width="1000"
-                height="1000"
-                src="https://sebpiq.github.io/WebPd_website/"
-            >
-            </iframe>
+            <img :class="$style.toolImage" :src="selectedTool.image_thumbnail" />
+            <div :class="$style.toolHeader">
+                <div :class="$style.toolMetadata">
+                    <h1 :class="$style.toolName">{{ selectedTool.name }}</h1>
+                    <p :class="$style.toolDescription">{{ selectedTool.description }}</p>
+                </div>
+                <div :class="$style.toolMetadata">
+                    <p :class="$style.toolAuthor"><b>Author: </b>{{ selectedTool.author }}</p>
+                    <p :class="$style.toolVersion"><b>Version: </b>{{ selectedTool.version }}</p>
+                    <a v-if="selectedTool.download_url" :class="$style.toolLink" :href="selectedTool.download_url" target="_blank">
+                        <button :class="$style.toolButton" >➜ Download</button>
+                    </a>
+                </div>
+            </div>
+            <div :class="$style.toolContent">
+                <iframe
+                    v-if="selectedTool.app_url"
+                    :class="$style.toolIframe"
+                    :src="selectedTool.app_url"
+                    title="Tool Iframe"
+                    width="1400"
+                    height="1000"
+                    scrolling="yes"
+                >
+                </iframe>
+            </div>
         </template>
         <template v-else>
-            <p class="tool__text">Tool not found</p>
+            <p :class="$style.toolText">Tool not found</p>
         </template>
     </div>
 </template>
